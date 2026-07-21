df6bc20b2924e81eef2a4a182a4e781a0b9d7a1a
Fany31-dev <s.rambeault@gmx.fr>
2026-07-21 15:41:00 +0200

    creation du provider et du router dans fichier main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./app/App";
+import "./styles/main.css";
 
-createRoot(document.getElementById('root')).render(
+// REDUX
+import { Provider } from "react-redux";
+import { store } from "./stores/store";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </StrictMode>,
-)
+);
