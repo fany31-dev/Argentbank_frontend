49a463b9e306b48895609c662537e20c8468d2af
Fany31-dev <s.rambeault@gmx.fr>
2026-07-22 16:39:31 +0200

    creation layout.jsx et ajout du layout dans le fichier App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
+import Layout from "../components/Layout.jsx";
 import Home from "../pages/Home.jsx";
 import Login from "../pages/Login.jsx";
 import Transactions from "../pages/Transactions.jsx";
@@ -7,12 +8,14 @@ import Error from "../pages/Error.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/transactions" element={<Transactions />} />
-      <Route path="*" element={<Error />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Layout>
   );
 }
 
