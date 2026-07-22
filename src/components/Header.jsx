c78431a022f55455c7abed96876bd7170d7c4393
Fany31-dev <s.rambeault@gmx.fr>
2026-07-22 16:38:31 +0200

    creation Component Header.jsx et Component Footer.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import LogoArgentBank from "@/assets/argentBankLogo.png";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <>
+      <nav className="main-nav">
+        <Link to="/" className="main-nav-logo">
+          <img
+            className="main-nav-logo-image"
+            src={LogoArgentBank}
+            alt="Argent Bank Logo"
+          />
+        </Link>
+        <h1 className="sr-only">Argent Bank</h1>
+        <div>
+          <Link to="/login" className="main-nav-item">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Header;
