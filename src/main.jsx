af9d505f2bb7fbcf917b2f217b0c795cab3c0154
Fany31-dev <s.rambeault@gmx.fr>
2026-09-14 17:30:35 +0200

    corerction de syntaxe sur les class / attributs et sur les balises semantiques

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./styles/main.css";
 
 // REDUX
 import { Provider } from "react-redux";
-import store from "./store/store.jsx";
+import store from "./store/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,22 +4,22 @@ function Login() {
   return (
     <main className="main bg-dark flex">
       <section className="sign-in-content">
-        <i class="fa fa-user-circle sign-in-icon"></i>
+        <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
         <form>
           <div className="input-wrapper">
-            <label htmlfor="username">Username</label>
+            <label htmlFor="username">Username</label>
             <input type="text" id="username" />
           </div>
           <div className="input-wrapper">
-            <label htmlfor="password">Password</label>
+            <label htmlFor="password">Password</label>
             <input type="password" id="password" />
           </div>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
-            <label htmlfor="remember-me">Remember me</label>
+            <label htmlFor="remember-me">Remember me</label>
           </div>
-          <button class="sign-in-button">Sign In</button>
+          <button className="sign-in-button">Sign In</button>
         </form>
       </section>
     </main>
